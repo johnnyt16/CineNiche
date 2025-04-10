770bda1ea95e63085976994e868316576389c244
jrigholman <120629795+jrigholman@users.noreply.github.com>
2025-04-10 16:50:21 -0600

    Update Program.cs

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -55,7 +55,8 @@ builder.Services.AddCors(options =>
                 "https://localhost:3000",  // HTTPS versions
                 "https://localhost:5173", 
                 "https://127.0.0.1:3000",
-                "https://127.0.0.1:5173"
+                "https://127.0.0.1:5173",
+                "https://localhost:5212"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
