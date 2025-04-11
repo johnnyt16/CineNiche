ea63e8dcc3cd4eaf2709db58ff173d27eb206a5f
johnnyt16 <johntaylormcc@gmail.com>
2025-04-11 04:09:11 -0600

    Commit made from VS Code Azure Static Web Apps

--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd65ac54253c765cc51a3198c70000b6035d3302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eddb9254955d1b3eedf25f820a78e196894f2cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
